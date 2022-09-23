2cbe073b77a2874969fc22da3c47544662266516
deeshavon <deja.greene@tlgcohort.com>
2022-09-23 12:19:52 -0700

    Lab 4.2 up until page 139 (lab book) completion, before lunch.

--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -12,15 +12,36 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class ShoppingCart<T> {
+public class ShoppingCart<T extends Product> {
+
   // storage for the cart's contents
   private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
-  
+
   public void addItem(T item) {
     items.add(item);
   }
-  
+
   public void removeItem(T item) {
     items.remove(item);
   }
+
+  public Collection<T> allItems() {
+    return Collections.unmodifiableCollection(items);
+  }
+
+  public int size() {
+    return items.size();  //this is an example of 'Composition'
+  }
+
+  public double total () {
+    double result = 0.0;
+    for (T item : items) {
+      result += item.getPrice();
+    }
+    return result;
+  }
+
+
+  
+
 }
